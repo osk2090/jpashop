7de6566961338ab3064401f2e163a073d09be664
osk2090 <osk2090@naver.com>
2022-03-02 22:59:09 +0900

    값타입과 불변 객체

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -24,14 +24,8 @@ public class JpaMain {
             member.setHomeAddress(address);
             em.persist(member);
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
-
-//            member.getHomeAddress().setCity("newCity");
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
 
             tx.commit();
         } catch (Exception e) {
